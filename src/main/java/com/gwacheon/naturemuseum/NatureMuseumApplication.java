023fef2430a8e1eabfed1b395509d2035bb9bf37
heogeonho <geonh1016@gmail.com>
2025-09-04 12:59:22 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/gwacheon/naturemuseum/NatureMuseumApplication.java
@@ -0,0 +1,13 @@
+package com.gwacheon.naturemuseum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatureMuseumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatureMuseumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gwacheon/naturemuseum/NatureMuseumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwacheon.naturemuseum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatureMuseumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
